fd229b2e5665f22d495c7f7711f7c1737cca38ca
Prayaan1711 <spyfyi1711@gmail.com>
2025-06-14 09:45:00 +0530

    1234

---
+++ Tuple operations.py
@@ -0,0 +1,19 @@
+tuplex = ("tuple", False, 3.2, 1)
+print(tuplex)
+
+tuplex = (4, 6, 8, 2, 3, 1)
+print(tuplex)
+
+tuplex = tuplex + (9, )
+print(tuplex)
+
+tuple1 = (50,10,60,70,50)
+print(tuple1.count(50))
+
+tuplex = (1,2,3,4,5,6,7,8,9,4)
+
+slice = tuplex[3:5]
+print(slice)
+
+slice = tuplex[:6]
+print(slice)
